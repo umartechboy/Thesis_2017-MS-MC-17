be6c43cff9fbe3e50b2d5006ff1c2db3e8ea9a9e
Muhammad Umar Hassan <umar.techboy@gmail.com>
2024-05-27 14:30:55 +0500

    Tool controls appearing and working

--- Tools/RotatingBezierSplineEditor/Gregor_WASM/Gregor_WASM/Services/FileManager.cs
+++ Tools/RotatingBezierSplineEditor/Gregor_WASM/Gregor_WASM/Services/FileManager.cs
@@ -5,14 +5,19 @@ namespace Gregor_WASM.Services
 {
     public class FileManager
     {
-        public static string Root { get; set; } = "";
+        public static string Root { get => "Gregor_WASM.Properties"; }
 
         public static byte[] GetBytes(string path)
         {
             var p = Path.Combine(Root, path.Trim('/').Trim('\\')).Replace("/", ".").Replace("\\", ".").Trim('.');
             var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            p = names.ToList().Find(n => n.ToLower() == p.ToLower());
             using (var s = Assembly.GetExecutingAssembly().GetManifestResourceStream(p))
             {
+                if (s == null)
+                {
+                    throw new FileNotFoundException($"The file {path} was not found in the embedded resources");
+                }
                 using (var ms = new MemoryStream())
                 {
                     s.CopyTo(ms);
